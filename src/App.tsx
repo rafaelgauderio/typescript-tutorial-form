6794c4450fe260f1a2818b9f5b363b22797c8ac7
Rafael de Luca <deluca1712@gmail.com>
2023-09-14 11:47:45 -0300

    Create project and base form

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h2>Criação do projeto</h2>
+      <div className="card">
+        <form>
+          <input
+            type="text"
+            placeholder='Inform your name'>
+          </input>
+          <input
+            type="text"
+            placeholder='Inform your profession'>
+          </input>
+          <input
+            type="text"
+            placeholder='Inform your age'>
+          </input>
+          <button>Show all data</button>
+
+        </form>
+      </div>
+    </>
+  )
+}
+
+export default App
